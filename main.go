2472b8adb08877ee504fa4c8bc77b2ee1a6d473e
ibobrova <ibobrova@ozon.ru>
2021-09-17 12:56:53 +0300

    work with structure

--- main.go
+++ main.go
@@ -5,8 +5,27 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
+		"equal: %d", u.name, u.age, u.money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(page http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(page, "Go is super easy!")
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Sasha")
+	fmt.Fprintf(page, bob.getAllInfo())
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +39,9 @@ func handleRequest() {
 }
 
 func main() {
+	//var bob User = ....
+	//bob = User{name: "Bob", age: 25, money: -50}
+	//bob = User{"Bob", 25, -50}
+
 	handleRequest()
 }
