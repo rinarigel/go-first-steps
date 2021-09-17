c34809f781c03578a8b02665f2c251a1598ed1f9
ibobrova <ibobrova@ozon.ru>
2021-09-17 13:53:03 +0300

    work with templates

--- main.go
+++ main.go
@@ -2,30 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
 }
 
 func (u User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
-		"equal: %d", u.name, u.age, u.money)
+		"equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
-func home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "Go is super easy!")
-	bob := User{"Bob", 25, -50, 4.2, 0.8}
-	bob.setNewName("Sasha")
-	fmt.Fprintf(page, bob.getAllInfo())
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bobb", 25, -50, 4.2, 0.8, []string{"Dance", "Films"}}
+	//fmt.Fprintf(w, "<b>Main text</b>")
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
+
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
