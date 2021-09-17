0a61f62d30c2f1df8b5166d53f8a1a35a89e96b0
ibobrova <ibobrova@ozon.ru>
2021-09-17 16:43:40 +0300

    work with database

--- main.go
+++ main.go
@@ -1,50 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html/template"
-	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
-	Name                  string
-	Age                   uint16
-	Money                 int16
-	Avg_grades, Happiness float64
-	Hobbies               []string
-}
-
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
-		"equal: %d", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.Name = newName
-}
-
-func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bobb", 25, -50, 4.2, 0.8, []string{"Dance", "Films"}}
-	//fmt.Fprintf(w, "<b>Main text</b>")
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, bob)
-
-}
-
-func contacts_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Contacts page")
-}
-
-func handleRequest() {
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts", contacts_page)
-	http.ListenAndServe(":8080", nil)
+	Id   uint16 `json:"id"`
+	Name string `json:"name"`
+	Age  uint16 `json:"age"`
 }
 
 func main() {
-	//var bob User = ....
-	//bob = User{name: "Bob", age: 25, money: -50}
-	//bob = User{"Bob", 25, -50}
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	handleRequest()
+	// Установка данных
+	insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES ('Bob', 35)")
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	//Выборка данных
+	res, err := db.Query("SELECT * FROM `users`")
+	if err != nil {
+		panic(err)
+	}
+
+	for res.Next() {
+		var user User
+		err = res.Scan(&user.Id, &user.Name, &user.Age)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
+	}
 }
