f7aa22bb37f9f981a08cbca794cd11c8856c6e9b
ibobrova <ibobrova@ozon.ru>
2021-09-17 18:11:43 +0300

    troubles

--- main.go
+++ main.go
@@ -1,45 +1,28 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	"html/template"
+	"net/http"
 )
 
-type User struct {
-	Id   uint16 `json:"id"`
-	Name string `json:"name"`
-	Age  uint16 `json:"age"`
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html",
+		"templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	// Установка данных
-	insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES ('Bob', 35)")
-	if err != nil {
-		panic(err)
-	}
-	defer insert.Close()
-
-	//Выборка данных
-	res, err := db.Query("SELECT * FROM `users`")
-	if err != nil {
-		panic(err)
-	}
-
-	for res.Next() {
-		var user User
-		err = res.Scan(&user.Id, &user.Name, &user.Age)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
-	}
+	handleFunc()
 }
